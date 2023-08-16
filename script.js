abacbf5bfe018dbf0edd51e556950341e0b1bc26
BugdaMatriz <140002506+BugdaMatriz@users.noreply.github.com>
2023-08-15 21:09:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLovePage() {
+    document.getElementById('main-content').style.display = 'none';
+    document.getElementById('love-page').style.display = 'block';
+}
+
+function showPleasePage() {
+    document.getElementById('main-content').style.display = 'none';
+    document.getElementById('please-page').style.display = 'block';
+}
